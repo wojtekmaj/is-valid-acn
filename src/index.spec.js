a2df6a065cd8d2e7f4283fa9ef57fb8e336f9f41
Wojciech Maj <kontakt@wojtekmaj.pl>
2019-07-25 13:32:36 +0200

    Add test for numeric input

--- src/index.spec.js
+++ src/index.spec.js
@@ -30,6 +30,11 @@ describe('isValidACN', () => {
 
     expect(result).toBe(false);
   });
+  it('returns true for valid numeric input', () => {
+    const result = isValidACN(143526096);
+
+    expect(result).toBe(true);
+  });
 
   it('returns true for valid input', () => {
     const result = isValidACN('010499966');
