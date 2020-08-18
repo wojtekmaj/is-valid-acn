3b0608665619c5cd3d928272df1efbc0fb089c65
Wojciech Maj <kontakt@wojtekmaj.pl>
2020-08-18 13:23:42 +0200

    Change if to guard clause

--- src/index.js
+++ src/index.js
@@ -8,19 +8,20 @@ export default function isValidACN(rawAcn) {
   // strip non-alphanumeric characters
   const acn = rawAcn.toString().replace(/[^a-zA-Z\d]/gi, '');
 
-  // check length is 9 digits
-  if (acn.length === 9) {
-    // apply ato check method
-    let sum = 0;
-    for (let position = 0; position < weights.length; position += 1) {
-      const weight = weights[position];
-      const digit = parseInt(acn[position], 10);
-      sum += weight * digit;
-    }
-
-    const checksum = (10 - (sum % 10)) % 10;
-    return checksum === parseInt(acn[8], 10);
+  // check if length is 9 digits
+  if (acn.length !== 9) {
+    return false;
   }
 
-  return false;
+  // apply ato check method
+  let sum = 0;
+  for (let position = 0; position < weights.length; position += 1) {
+    const weight = weights[position];
+    const digit = parseInt(acn[position], 10);
+    sum += weight * digit;
+  }
+
+  const checksum = (10 - (sum % 10)) % 10;
+
+  return checksum === parseInt(acn[8], 10);
 }
